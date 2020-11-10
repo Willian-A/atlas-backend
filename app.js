8895bba0e4cd7ca09d120dd78e153eec87d11d3b
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 00:45:18 -0300

    teste

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ app.use(
     credentials: true,
   })
 );
-
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 const options = {
   key: fs.readFileSync("./cert/selfsigned.key", "utf8"),
   cert: fs.readFileSync("./cert/selfsigned.crt", "utf8"),
