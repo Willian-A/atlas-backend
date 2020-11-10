e27c464fb7da32b25a52cb6cf8cf4269c1a6af76
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 00:59:52 -0300

    last

--- app.js
+++ app.js
@@ -16,19 +16,12 @@ const cartRoute = require("./routes/cart.js");
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type, Accept,Authorization,Origin"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(
+  cors({
+    origin: false,
+    credentials: true,
+  })
+);
 
 const options = {
   key: fs.readFileSync("./cert/selfsigned.key", "utf8"),
