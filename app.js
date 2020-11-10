3def7c7297eacfe14c4ec574b3757e859437db2e
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 15:48:46 -0300

    teste

--- app.js
+++ app.js
@@ -16,16 +16,7 @@ const cartRoute = require("./routes/cart.js");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  app.use(cors());
-  next();
-});
+app.use(cors());
 
 const options = {
   key: fs.readFileSync("./cert/selfsigned.key", "utf8"),
