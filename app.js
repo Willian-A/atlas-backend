e41b4b38d40b2d8f353528364409948d90d9d6d3
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 16:36:36 -0300

    teste

--- app.js
+++ app.js
@@ -15,31 +15,25 @@ const productRoute = require("./routes/product.js");
 const cartRoute = require("./routes/cart.js");
 
 const app = express();
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+
+app.use(express.static("public"));
+
+app.get("/hello", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send("Hello World");
+});
+
 const options = {
   key: fs.readFileSync("./cert/selfsigned.key", "utf8"),
   cert: fs.readFileSync("./cert/selfsigned.crt", "utf8"),
 };
 const credentials = { key: options.key, cert: options.cert };
-
-const routes = Router();
-
-routes.get("/a", async (req, res) => {
-  console.log(req);
-  res.send("Hello World");
-});
-
 // Metodos HTTP: GET, POST, PUT, DELETE
 
 // Tipos de parametos:
 // Query Params: request.query (Filstros, ordenação, paginação, ...)
 // Route Params: request.params (identificar um recurso na alteração ou remoção)
 // Body: request.body
-http.createServer(app).listen(8080);
-https.createServer(credentials, app).listen(process.env.PORT || 3333);
-console.log(`Listening to: ${process.env.PORT}`);
+app.listen(3333, () => {
+  console.log("alive");
+});
