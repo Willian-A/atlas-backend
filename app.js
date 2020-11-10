986e45b741e9133693a1b0f410612aca21c45d40
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 00:57:30 -0300

    aa

--- app.js
+++ app.js
@@ -16,34 +16,19 @@ const cartRoute = require("./routes/cart.js");
 
 const app = express();
 
-var whitelist = [
-  "https://frontend-tcc.vercel.app/",
-  "http://localhost:3000",
-  "https://frontend-tcc.vercel.app",
-]; //white list consumers
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "device-remember-token",
-    "Access-Control-Allow-Origin",
-    "Origin",
-    "Accept",
-  ],
-};
-
-app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 const options = {
   key: fs.readFileSync("./cert/selfsigned.key", "utf8"),
