cf85632f53554ddc4246ee56f90082fa25c9e7ce
Willian Almeida <titangames.tg0@gmail.com>
2020-11-10 00:53:21 -0300

    teste

--- app.js
+++ app.js
@@ -16,27 +16,16 @@ const cartRoute = require("./routes/cart.js");
 
 const app = express();
 
-var allowedOrigins = [
-  "https://localhost:3000",
-  "https://frontend-tcc.vercel.app/",
-];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
     credentials: true,
   })
 );
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 const options = {
   key: fs.readFileSync("./cert/selfsigned.key", "utf8"),
   cert: fs.readFileSync("./cert/selfsigned.crt", "utf8"),
