92e7c66fa5272572c08010fb4d85d0ce472d8350
Willian Almeida <titangames.tg0@gmail.com>
2020-12-05 13:09:14 -0300

    asdasd

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
 app.get("/", async (req, res) => {
   res.sendStatus("Servidor Backend");
 });
