f15f7d78671268dab021e449e8227ae9ba1e68e3
Willian Almeida <titangames.tg0@gmail.com>
2020-12-06 14:09:21 -0300

    update

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ const cartRoute = require("./routes/cart.js");
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
