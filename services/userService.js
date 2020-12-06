1ff456d526c05448f43bd0a32b9bfc8a648d1c9d
Willian Almeida <titangames.tg0@gmail.com>
2020-12-06 14:45:08 -0300

    logged debug

--- services/userService.js
+++ services/userService.js
@@ -71,14 +71,7 @@ module.exports = class UserService {
   }
 
   async logged(profile) {
-    if (profile) {
-      let isValid = jwt.verify(profile, process.env.SECRET, (err, decoded) => {
-        return bcrypt.compareSync("true", decoded.token);
-      });
-      console.log(isValid);
-      return { error: false };
-    } else {
-      return { error: true, HTTPcode: 403 };
-    }
+    console.log(profile);
+    return { error: true, HTTPcode: 403 };
   }
 };
